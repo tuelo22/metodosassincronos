2617aa912bab2a18527fe14510dc95983d069fc6
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2025-04-02 00:35:23 -0300

    Subindo alterações

---
+++ metodosassincronos/metodosassincronos.domain/DTO/DeckResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace metodosassincronos.domain.DTO
+{
+    public struct DeckResponseDTO
+    {
+        public bool Success { get; set; } 
+        public string DeckId { get; set; } 
+        public int Remaining { get; set; } 
+        public bool Shuffled { get; set; } 
+    }
+
+}
